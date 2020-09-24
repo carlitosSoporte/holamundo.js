a92a0022fb5b3629a6f0bd58dbde2d8c2a605d5c
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-09-24 11:25:45 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,9 @@
+let nombre="carlos";
+let edad = 5;
+let estatura = 1.62;
+let estaVivo = true;
+const PI = 3.141516;
+
+console.log("hola mundo");
+console.log("el nombre es "+nombre);
+console.log(`su nombre es ${nombre} y su estatura es ${estatura}`);
