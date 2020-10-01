5f6054248569e5208c139435fb0fdc39ca93e79d
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-10-01 11:17:41 -0500

    ejercicio terminado

--- controladorDOM.js
+++ controladorDOM.js
@@ -1,2 +1,13 @@
-let tituloTarjeta2 = Document.getElementById('titulo2');
-console.log(tituloTarjeta2.log);
+let tituloTarjeta2 = document.getElementById('titulo2');
+let audioVariable = document.getElementById('audio1');
+let botonPista1 = document.getElementById('pista1');
+let botonPista2 = document.getElementById('pista2');
+let textoApp = document.getElementById("textoPrincipal")
+let imagenTarjeta1 = document.getElementById("imagen1");
+let imagenTarjeta2 = document.getElementById("imagen2");
+
+tituloTarjeta2.textContent = "Banda Coldplay";
+textoApp.textContent ="Bandas de rock";
+audioVariable.src = "recursos/audio2.mp3";
+imagenTarjeta1.src = "recursos/imagen3.jpg";
+imagenTarjeta2.src = "recursos/imagen3.jpg";
