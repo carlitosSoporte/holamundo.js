7acba5d0f960c1fa3053c4dc2a43a6d5ea247195
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-10-01 08:58:34 -0500

    se agregan recursos de clase

--- controlador2.js
+++ controlador2.js
@@ -3,16 +3,19 @@ let numeroContrato ="1145312";
 let estratoEconomico = "5";
 let valorCuentaServicios = 250000;
 
-//proceso
-if(estratoEconomico == "1"){
-    console.log("40%");
-}
-else if(estratoEconomico == "2"){
-    console.log("30%");
-}
-else if(estratoEconomico == "3"){
-    console.log("20%");
-}
-else{
-    console.log("no tiene descuento");
+switch(estratoEconomico){
+    case 1:
+        console.log("40%");
+        break;
+    case 2:
+        console.log("30%");
+        break;
+
+    case 3:
+        console.log("20%");
+        break;
+    default:
+        console.log("no tiene descuento");
+        break;
+
 }
